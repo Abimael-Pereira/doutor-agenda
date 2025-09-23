18d9dce433a7a8ba32bcd5161039bacf73b12f35
Abimael <abimael.spdev@gmail.com>
2025-09-23 11:05:15 -0400

    feat: add custom error message for existing user

--- src/app/authentication/components/sign-up-form.tsx
+++ src/app/authentication/components/sign-up-form.tsx
@@ -2,6 +2,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -53,6 +54,14 @@ const SignUpForm = () => {
       {
         onSuccess: () => {
           router.push("/dashboard");
+        },
+        onError: (ctx) => {
+          if (ctx.error.code === "USER_ALREADY_EXISTS_USE_ANOTHER_EMAIL") {
+            toast.error("Usuário já existe. Tente outro email.");
+            return;
+          }
+
+          toast.error("Erro ao criar conta. Tente novamente.");
         },
       },
     );
