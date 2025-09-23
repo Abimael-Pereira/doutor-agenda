fb0273d9fcb8fd71a644f1283e75fbb820bb9649
Abimael <abimael.spdev@gmail.com>
2025-09-23 15:29:11 -0400

    feat: implement authentication flow with sign-out functionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,34 @@
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50 p-4">
+      <h1 className="mb-4 text-2xl font-bold">Dashboard</h1>
+      {session ? (
+        <>
+          <p className="text-lg">
+            Bem-vindo, {session.user.name}, {session.user.email}!
+          </p>
+          <SignOutButton />
+        </>
+      ) : (
+        <p className="text-lg">Você não está autenticado.</p>
+      )}
+    </div>
+  );
 };
 
 export default DashboardPage;
