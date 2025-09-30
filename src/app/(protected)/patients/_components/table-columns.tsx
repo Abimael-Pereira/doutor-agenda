001c629e282622fd5292db89a3a9a27099396d4a
Abimael <abimael.spdev@gmail.com>
2025-09-30 09:34:34 -0400

    feat: add patients table

---
+++ src/app/(protected)/patients/_components/table-columns.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { patientsTable } from "@/db/schema";
+
+type Patients = typeof patientsTable.$inferSelect;
+
+export const patientsTableColumns: ColumnDef<Patients>[] = [
+  {
+    id: "name",
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    id: "email",
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    id: "phoneNumber",
+    accessorKey: "phoneNumber",
+    header: "Telefone",
+  },
+  {
+    id: "sex",
+    accessorKey: "sex",
+    header: "Sexo",
+    cell: ({ row }) => (row.original.sex === "male" ? "Masculino" : "Feminino"),
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const patient = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <MoreVerticalIcon />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <EditIcon />
+              Editar
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <TrashIcon />
+              Excluir
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
